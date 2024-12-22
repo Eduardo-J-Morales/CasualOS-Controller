845ff8d067286d7816634c5022a73f2f3a3a0acc
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:52:07 -0400

    Update data.js

--- @onAnyBotClicked/@HandleBotClicked/@EnableControllerForBot/@StartUpdate/@Update/@HandleMovement/@GetNormalizeInputDirection/data.js
+++ @onAnyBotClicked/@HandleBotClicked/@EnableControllerForBot/@StartUpdate/@Update/@HandleMovement/@GetNormalizeInputDirection/data.js
@@ -1,1 +1,16 @@
-
+const input = {
+    up: os.getInputState("keyboard","W") || os.getInputState("keyboard","w") || os.getInputState("keyboard","ArrowUp"),
+    down: os.getInputState("keyboard","S") || os.getInputState("keyboard","s") || os.getInputState("keyboard","ArrowDown"),
+    left: os.getInputState("keyboard","A") || os.getInputState("keyboard","a") || os.getInputState("keyboard","ArrowLeft"),
+    right: os.getInputState("keyboard","D") || os.getInputState("keyboard","d") || os.getInputState("keyboard","ArrowRight")
+}
+const direction = new Vector3(
+    (input.right ? 1 : 0) - (input.left ? 1 : 0),
+    (input.up ? 1 : 0) - (input.down ? 1 : 0),
+    0
+)
+if(direction.length() > 0)
+{
+    return direction.normalize();
+}
+return new Vector3(0, 0, 0);
