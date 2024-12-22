9f48bfc40d253b4d93419f0ebd3ad1c948f2bbc0
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:46:48 -0400

    Update data.js

--- @onAnyBotClicked/@HandleBotClicked/@DisableController/data.js
+++ @onAnyBotClicked/@HandleBotClicked/@DisableController/data.js
@@ -1,1 +1,7 @@
+@DisableController
 
+// Stop the update process for the current bot (thisBot)
+thisBot.StopUpdate();
+
+// Set the current controlled bot to null, indicating that no bot is being controlled by 'thisBot'
+thisBot.vars.currentControlledBot = null;
