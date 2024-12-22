5024ad9d4b702c331dbe0ae96e7616cbdaaf1a85
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:46:14 -0400

    Update data.js

--- @onAnyBotClicked/@HandleBotClicked/@EnableControllerForBot/data.js
+++ @onAnyBotClicked/@HandleBotClicked/@EnableControllerForBot/data.js
@@ -1,1 +1,8 @@
+// Destructure 'bot' from 'that' (i.e., assign 'bot' from the 'that' object)
+const {bot} = that;
 
+// If there is no currently controlled bot, start the update process for 'thisBot'
+if(!thisBot.vars.currentControlledBot) thisBot.StartUpdate();
+
+// Set the current controlled bot to the 'bot' from 'that', meaning 'thisBot' is now controlling this new bot
+thisBot.vars.currentControlledBot = bot;
