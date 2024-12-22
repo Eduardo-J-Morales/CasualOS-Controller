f8d0a8c747cd3a0bdcfd5481880421750e698fa1
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:39:39 -0400

    Update Part13.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part13.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part13.js
@@ -1,1 +1,8 @@
-
+globalThis.botFacesByVertices = {
+    bottom: [0, 1, 2, 3],
+    right: [3, 2, 6, 7],
+    top: [7, 6, 5, 4],
+    left: [4, 5, 1, 0],
+    front: [1, 5, 6, 2],
+    back: [4, 0, 3, 7]
+}
