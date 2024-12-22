890544e527f8f5dc94f724b74e4000c1c5dd1a50
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:29:56 -0400

    Update data.js

--- @onKeyDown/data.js
+++ @onKeyDown/data.js
@@ -1,1 +1,12 @@
-
+// Check if the first element in the 'keys' array (that.keys[0]) is either "c" or "C"
+if (that.keys[0] === "c" || that.keys[0] === "C") {
+    // If the key pressed is "c" or "C", call the 'ToggleBotPicker' method on the 'thisBot' object
+    // This function likely toggles the state of a bot picker (e.g., showing or hiding a bot selection menu)
+    thisBot.ToggleBotPicker();
+}
+// If the key is not "c" or "C", check if it's a spacebar
+else if (that.keys[0] === " ") {
+    // If the key pressed is a space, call the 'TryAddVerticalSpeed' method on the 'thisBot' object
+    // This function likely attempts to apply or modify the bot's vertical speed (e.g., jump or fly)
+    thisBot.TryAddVerticalSpeed();
+}
