0f50beed53cedd0c6f9e54f893fd480aa11c6086
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:35:55 -0400

    Update Part5.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part5.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part5.js
@@ -1,1 +1,14 @@
-
+function GetBotVertices(limits) {
+    // Define the 8 vertices of a box-like shape based on its limits (min/max values)
+    let vertices = [
+        new Vector3(limits.x.max, limits.y.max, limits.z.min), // Top-front-right
+        new Vector3(limits.x.max, limits.y.min, limits.z.min), // Bottom-front-right
+        new Vector3(limits.x.min, limits.y.min, limits.z.min), // Bottom-front-left
+        new Vector3(limits.x.min, limits.y.max, limits.z.min), // Top-front-left
+        new Vector3(limits.x.max, limits.y.max, limits.z.max), // Top-back-right
+        new Vector3(limits.x.max, limits.y.min, limits.z.max), // Bottom-back-right
+        new Vector3(limits.x.min, limits.y.min, limits.z.max), // Bottom-back-left
+        new Vector3(limits.x.min, limits.y.max, limits.z.max)  // Top-back-left
+    ]
+    return vertices; // Return the array of 8 vertices
+}
