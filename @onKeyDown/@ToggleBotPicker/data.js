9698a3b451dfab27134c43186dfe650398c0f515
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:30:43 -0400

    Update data.js

--- @onKeyDown/@ToggleBotPicker/data.js
+++ @onKeyDown/@ToggleBotPicker/data.js
@@ -1,1 +1,3 @@
 
+setTagMask(thisBot, "isBotPickerEnabled", !thisBot.masks.isBotPickerEnabled);
+os.toast(`Bot picker ${thisBot.masks.isBotPickerEnabled ? `On. Click any bot to control it` : `Off`}`);
