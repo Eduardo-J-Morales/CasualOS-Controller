8d988c8bf2d040162b0d8d1770b928cf66707fe5
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:36:42 -0400

    Update Part7.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part7.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part7.js
@@ -1,1 +1,4 @@
-
+function FixQuaternionHemisphere(start, target) {
+    const dot = QuaternionDot(start, target); // Compute the dot product of the quaternions
+    return dot < 0 ? NegateQuaternion(target) : target; // If dot product is negative, negate the quaternion
+}
