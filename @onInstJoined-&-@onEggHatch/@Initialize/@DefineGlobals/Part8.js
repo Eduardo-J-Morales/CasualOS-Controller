0d6041705b7644bd03604158a3a06a4213c53077
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:37:08 -0400

    Update Part8.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part8.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part8.js
@@ -1,1 +1,8 @@
-
+function QuaternionDot(quatA, quatB) {
+    return (
+        quatA._q.x * quatB._q.x +
+        quatA._q.y * quatB._q.y +
+        quatA._q.z * quatB._q.z +
+        quatA._q.w * quatB._q.w
+    );
+}
