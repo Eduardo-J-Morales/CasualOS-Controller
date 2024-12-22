c9ad002b065b665d0438f288a01cc1d60e9539f7
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:31:36 -0400

    Update data.js

--- @onKeyDown/@TryAddVerticalSpeed/data.js
+++ @onKeyDown/@TryAddVerticalSpeed/data.js
@@ -1,1 +1,7 @@
-
+const jumpSpeed = 10;
+if(!thisBot.vars.currentControlledBot.masks.isJumping)
+{
+  setTagMask(thisBot.vars.currentControlledBot, "isJumping", true);
+  const newVerticalVelocity = (thisBot.vars.currentControlledBot.masks.verticalVelocity ?? 0) + jumpSpeed;
+  setTagMask(thisBot.vars.currentControlledBot, "verticalVelocity", newVerticalVelocity);
+}
