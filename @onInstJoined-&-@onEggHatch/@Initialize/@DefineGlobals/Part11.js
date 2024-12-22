308d9eb8eccd99cc8403242b30f98519cdc1a4ee
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:38:15 -0400

    Update Part11.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part11.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part11.js
@@ -1,1 +1,13 @@
-
+globalThis.GetNormalsByVertices = GetNormalsByVertices;
+globalThis.DetectGap = DetectGap;
+globalThis.GetProjectedPoints = GetProjectedPoints;
+globalThis.FilterParallelVectors = FilterParallelVectors;
+globalThis.GetBotScales = GetBotScales;
+globalThis.GetBotRotations = GetBotRotations;
+globalThis.GetBotLimits = GetBotLimits;
+globalThis.GetBotVertices = GetBotVertices;
+globalThis.QuaternionToEuler = QuaternionToEuler;
+globalThis.FixQuaternionHemisphere = FixQuaternionHemisphere;
+globalThis.QuaternionDot = QuaternionDot;
+globalThis.NegateQuaternion = NegateQuaternion;
+globalThis.GetNormalByFace = GetNormalByFace;
