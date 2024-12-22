f10f9b8897bd0979539a2d3ec889cf568a7eb3ec
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:37:36 -0400

    Update Part9.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part9.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part9.js
@@ -1,1 +1,6 @@
-
+function NegateQuaternion(quaternion) {
+    const { x, y, z, w } = quaternion._q;
+    const inverseQuaternion = new Quaternion(-x, -y, -z, -w); // Negate each component of the quaternion
+    const negated = new Rotation(inverseQuaternion); // Wrap the negated quaternion in a Rotation object
+    return negated;
+}
