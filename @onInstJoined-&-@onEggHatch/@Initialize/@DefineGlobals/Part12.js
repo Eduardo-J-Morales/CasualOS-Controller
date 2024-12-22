c396ea13e9dcafb4227e73b85dab844a73e0f8a3
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:38:37 -0400

    Update Part12.js

--- @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part12.js
+++ @onInstJoined-&-@onEggHatch/@Initialize/@DefineGlobals/Part12.js
@@ -1,1 +1,6 @@
-
+globalThis.VectorUp = new Vector3(0, 0, 1);
+globalThis.VectorDown = new Vector3(0, 0, -1);
+globalThis.VectorRight = new Vector3(1, 0, 0);
+globalThis.VectorLeft = new Vector3(-1, 0, 0);
+globalThis.VectorForward = new Vector3(0, 1, 0);
+globalThis.VectorBackward = new Vector3(0, -1, 0);
