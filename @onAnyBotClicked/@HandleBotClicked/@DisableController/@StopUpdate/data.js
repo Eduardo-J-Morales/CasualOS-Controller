c4f66e83cc332166dd94a046abb5cf950761faea
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:47:46 -0400

    Update data.js

--- @onAnyBotClicked/@HandleBotClicked/@DisableController/@StopUpdate/data.js
+++ @onAnyBotClicked/@HandleBotClicked/@DisableController/@StopUpdate/data.js
@@ -1,1 +1,9 @@
+// Check if 'thisBot.masks.updateInterval' exists (i.e., there is an active interval)
+if(thisBot.masks.updateInterval) {
+    
+    // If an interval is active, clear it to stop the recurring action
+    clearInterval(thisBot.masks.updateInterval);
 
+    // Remove or reset the 'updateInterval' tag/mask from 'thisBot'
+    setTagMask(thisBot, "updateInterval", null);
+}
