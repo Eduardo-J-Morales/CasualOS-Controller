f491dda6a3bef483cc2c3ab26039a9073086ee94
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-22 16:49:18 -0400

    Update data.js

--- @onAnyBotClicked/@HandleBotClicked/@EnableControllerForBot/@StartUpdate/@Update/data.js
+++ @onAnyBotClicked/@HandleBotClicked/@EnableControllerForBot/@StartUpdate/@Update/data.js
@@ -1,1 +1,7 @@
+const {deltaTime} = that; // Destructure deltaTime from the 'that' object
 
+// Check if the currentControlledBot exists (i.e., there is a bot currently being controlled)
+if(thisBot.vars.currentControlledBot) {
+    // If a bot is currently being controlled, call the HandleMovement method and pass the deltaTime
+    thisBot.HandleMovement({deltaTime});
+}
